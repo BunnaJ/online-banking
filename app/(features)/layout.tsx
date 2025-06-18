7bf73cbf68de9074cb62b16a40ed42eda48f0636
Bunna <BunnaJ>
2025-06-18 19:33:16 +0100

    layout

--- app/(features)/layout.tsx
+++ app/(features)/layout.tsx
@@ -1,17 +1,15 @@
-// app/dashboard/layout.tsx
 "use client";
 import SideNav from "@/components/sidenav/SideNav";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex ">
-      <aside className="w-[300px] pt-10">
+    <div className="flex min-h-screen overflow-hidden">
+      <aside className="w-[300px] pt-10 shrink-0">
         <SideNav />
       </aside>
-      <main className="flex-1 p-10 border-l-2 pt-20">
+      <main className="flex-1 p-10 border-l-2 pt-20 overflow-auto">
         {children}
       </main>
-      
     </div>
   );
 }
