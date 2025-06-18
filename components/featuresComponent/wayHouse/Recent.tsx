56526b211acdfd9aca7a28fd5ebe26ca14b4f014
Bunna <BunnaJ>
2025-06-18 21:56:21 +0100

    changed

--- components/featuresComponent/wayHouse/Recent.tsx
+++ components/featuresComponent/wayHouse/Recent.tsx
@@ -11,7 +11,7 @@ export default function Recent() {
         <AccountOverview/>
         <SpendingChart />
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between pt-11">
         <h1 className="font-bold text-xl">Recent transactions</h1>
         <div className="border w-[100px] p-2 text-sm border-[var(--main)] rounded cursor-pointer text-center hover:bg-indigo-50 ">
           View All
